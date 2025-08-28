b17df3e54adfc2b9c9fcc02e9276a013bf2a50d2
moataz bellah <moatazbellah70@gmail.com>
2025-08-28 23:41:52 +0300

    added notFound and aboutUs components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { RecipesComponent } from './Components/recipes/recipes.component';
 import { AuthComponent } from '../app//Components//auth//auth.component';
 import { RecipeDetailsComponent } from './Components/recipe-details/recipe-details.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
 
 export const routes: Routes = [
   { path: 'auth', component: AuthComponent },
+  { path: 'about-us', component: AboutUsComponent },
   { path: 'recipes', component: RecipesComponent },
-  { path: 'recipes/:id', component: RecipeDetailsComponent},
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth' },
+  { path: 'recipe/:id', component: RecipeDetailsComponent},
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
